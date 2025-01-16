12420d3146e1babc23977d6d39204ec49256d14f
samrouth1234 <bichsamroth@gmail.com>
2025-01-16 17:04:44 +0700

    Set up project sping boot with keycloak

---
+++ src/main/java/com/co/keycloakbasic/KeycloakBasicApplication.java
@@ -0,0 +1,13 @@
+package com.co.keycloakbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeycloakBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/co/keycloakbasic/KeycloakBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.keycloakbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
